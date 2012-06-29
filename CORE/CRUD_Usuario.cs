3e33062cf50df09a41e97c2edc3d16c4b4c92953
kote <maria.osorios@usach.cl>
2012-06-29 10:45:23 -0400

    Método Usuario
    Quería ver si funcionaba XD

--- CORE/CRUD_Usuario.cs
+++ CORE/CRUD_Usuario.cs
@@ -7,5 +7,19 @@ namespace CORE
 {
     public class CRUD_Usuario
     {
+
+        public static List<USUARIO> getAll()
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                var usuario = from usu in ctx.ListaUsuario
+                              
+                               orderby usu.nombre
+                               select usu;
+
+                return usuario.ToList<USUARIO>();
+            }
+        }
+
     }
 }
