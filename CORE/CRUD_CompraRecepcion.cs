8fa297bba37241a86d7afad30f6dfb2a2e403332
camiloonly <camiloonly@msn.com>
2012-06-29 02:13:18 -0400

    funciona po

--- CORE/CRUD_CompraRecepcion.cs
+++ CORE/CRUD_CompraRecepcion.cs
@@ -7,5 +7,71 @@ namespace CORE
 {
     public class CRUD_CompraRecepcion
     {
+        public static List<COMPRA_RECEPCION> getAll()
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                var CompraRecepcion = from c_r in ctx.ListaCompraRecepcion
+                             orderby c_r.SC_ID
+                             select c_r;
+                return CompraRecepcion.ToList<COMPRA_RECEPCION>();
+            }
+
+        }
+
+        public static void Create(COMPRA_RECEPCION c_r)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ctx.ListaCompraRecepcion.InsertOnSubmit(c_r);
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static COMPRA_RECEPCION Read(int cr_sc_id, int cr_rm_id )
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                try
+                {
+                    var CompraRecepcion = from c_r in ctx.ListaCompraRecepcion
+                                          where (c_r.SC_ID.Equals(cr_sc_id) && c_r.RM_ID.Equals(cr_rm_id))
+                                          select c_r;
+                    return CompraRecepcion.First<COMPRA_RECEPCION>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void Update(COMPRA_RECEPCION cr_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                COMPRA_RECEPCION CompraRecepcion = (from c_r in ctx.ListaCompraRecepcion
+                                                    where (c_r.SC_ID.Equals(cr_upd.SC_ID) && c_r.RM_ID.Equals(cr_upd.RM_ID))
+                                                    select c_r).First<COMPRA_RECEPCION>();
+
+                CompraRecepcion.SC_ID = cr_upd.SC_ID;
+                CompraRecepcion.RM_ID = cr_upd.RM_ID;
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static void Delete(COMPRA_RECEPCION cr_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                COMPRA_RECEPCION borrar = (from c_r in ctx.ListaCompraRecepcion
+                                           where (c_r.SC_ID.Equals(cr_upd.SC_ID) && c_r.RM_ID.Equals(cr_upd.RM_ID))
+                                           select c_r).First<COMPRA_RECEPCION>();
+
+                ctx.ListaCompraRecepcion.DeleteOnSubmit(borrar);
+                ctx.SubmitChanges();
+            }
+        }
+
     }
 }
--- CORE/CRUD_Estado.cs
+++ CORE/CRUD_Estado.cs
@@ -7,5 +7,71 @@ namespace CORE
 {
     public class CRUD_Estado
     {
+        public static List<ESTADO> getAll()
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                var estado = from e in ctx.ListaEstado
+                             orderby e.E_ID
+                             select e;
+
+                return estado.ToList<ESTADO>();
+            }
+        }
+
+        public static void Create(ESTADO estado_new)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ctx.ListaEstado.InsertOnSubmit(estado_new);
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static ESTADO Read(int estado_id)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                try
+                {
+                    var estado = from e in ctx.ListaEstado
+                                 where e.E_ID.Equals(estado_id)
+                                 select e;
+                    return estado.First<ESTADO>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void Update(ESTADO estado_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ESTADO estado = (from e in ctx.ListaEstado
+                                 where e.E_ID.Equals(estado_upd.E_ID)
+                                 select e).First<ESTADO>();
+
+                estado.E_NOMBRE = estado_upd.E_NOMBRE;
+                estado.E_DESCRIPCION = estado_upd.E_DESCRIPCION;
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static void Delete(ESTADO estado_del)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ESTADO borrar = (from e in ctx.ListaEstado
+                                 where e.E_ID.Equals(estado_del.E_ID)
+                                 select e).First<ESTADO>();
+
+                ctx.ListaEstado.DeleteOnSubmit(borrar);
+                ctx.SubmitChanges();
+            }
+        }
+
     }
 }
