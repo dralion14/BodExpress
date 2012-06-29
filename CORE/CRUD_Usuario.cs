91ad7fcb180caef43249260b583b1c7573c8c32b
camiloonly <camiloonly@msn.com>
2012-06-29 11:06:27 -0400

    Crud USUARIOS
    delete

--- CORE/CRUD_Usuario.cs
+++ CORE/CRUD_Usuario.cs
@@ -21,5 +21,17 @@ namespace CORE
             }
         }
 
+        public static void Delete(USUARIO usuario)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                USUARIO borrar = (from u in ctx.ListaUsuario
+                                   where u.nombre.Equals(usuario.nombre)
+                                   select u).First<USUARIO>();
+
+                ctx.ListaUsuario.DeleteOnSubmit(borrar);
+                ctx.SubmitChanges();
+            }
+        }
     }
 }
