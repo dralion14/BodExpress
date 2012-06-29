1265db41943c99c1954838fcfd813d8d998b9dac
kote <maria.osorios@usach.cl>
2012-06-29 11:22:10 -0400

    (8)Cómo te va mi amooorrrr, cómo te va.....(8)
    prueba 2

--- CORE/CRUD_Usuario.cs
+++ CORE/CRUD_Usuario.cs
@@ -19,6 +19,47 @@ namespace CORE
                 return usuario.ToList<USUARIO>();
             }
         }
+        public static void Create(USUARIO usu_new)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ctx.ListaUsuario.InsertOnSubmit(usu_new);
+                ctx.SubmitChanges();
+            }
+        }
+        public static USUARIO Read(int nombre)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                try
+                {
+                    var usuario = from usu in ctx.ListaUsuario
+                                   where usu.nombre.Equals(nombre)
+                                   select usu;
+                    return usuario.First<USUARIO>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+        public static void Update(USUARIO usu_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+               USUARIO usuario = (from usu in ctx.ListaUsuario
+                                     where usu.nombre.Equals(usu_upd.nombre)
+                                     select usu).First<USUARIO>();
+
+                usuario.nombre = usu_upd.nombre;
+                usuario.pass = usu_upd.pass;
+                usuario.area = usu_upd.area;
+
+                ctx.SubmitChanges();
+            }
+        }
+
 
 
 
