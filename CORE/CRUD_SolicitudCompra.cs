59bcd39a6930230f3dfb2e9d37fc06ecc4338cdb
kote <maria.osorios@usach.cl>
2012-07-02 19:27:19 -0400

    KOtetita: Solicitud de Compra y Solicitud de Compra Detalle
    Holi ^^

--- CORE/CRUD_SolicitudCompra.cs
+++ CORE/CRUD_SolicitudCompra.cs
@@ -7,5 +7,74 @@ namespace CORE
 {
     public class CRUD_SolicitudCompra
     {
+        public static List<SOLICITUD_COMPRA> getAll()
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                var solicitud_compra = from sol_comp in ctx.ListaSolicitudCompra
+                               
+                               orderby sol_comp.SC_ID
+                               select sol_comp;
+
+                return solicitud_compra.ToList<SOLICITUD_COMPRA>();
+            }
+        }
+
+
+        public static void Create(SOLICITUD_COMPRA sol_comp_new)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ctx.ListaSolicitudCompra.InsertOnSubmit(sol_comp_new);
+                ctx.SubmitChanges();
+            }
+        }
+
+
+        public static SOLICITUD_COMPRA Read(int sol_comp_id)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                try
+                {
+                    var solicitud_compra= from sol_comp in ctx.ListaSolicitudCompra
+                                   where sol_comp.SC_ID.Equals(sol_comp_id)
+                                   select sol_comp;
+                    return solicitud_compra.First<SOLICITUD_COMPRA>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void Update(SOLICITUD_COMPRA sol_comp_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                SOLICITUD_COMPRA solicitud_compra = (from sol_comp in ctx.ListaSolicitudCompra
+                                                     where sol_comp.SC_ID.Equals(sol_comp_upd.SC_ID)
+                                                     select sol_comp).First<SOLICITUD_COMPRA>();
+
+                solicitud_compra.SC_FECHA = sol_comp_upd.SC_FECHA;
+                solicitud_compra.E_ID = sol_comp_upd.E_ID;
+               
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static void Delete(SOLICITUD_COMPRA sol_comp_del)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                SOLICITUD_COMPRA borrar = (from sol_comp in ctx.ListaSolicitudCompra
+                                           where sol_comp.SC_ID.Equals(sol_comp_del.SC_ID)
+                                           select sol_comp).First<SOLICITUD_COMPRA>();
+
+                ctx.ListaSolicitudCompra.DeleteOnSubmit(borrar);
+                ctx.SubmitChanges();
+            }
+        }
     }
 }
--- CORE/CRUD_SolicitudCompraDetalle.cs
+++ CORE/CRUD_SolicitudCompraDetalle.cs
@@ -7,5 +7,74 @@ namespace CORE
 {
     public class CRUD_SolicitudCompraDetalle
     {
+
+        public static List<DETALLE_SOLICITUD_COMPRA> getAll()
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                var detalle_sol_comp = from det_sol_comp in ctx.ListaSolicitudCompraDetalle
+                               
+                               orderby det_sol_comp.SC_ID,det_sol_comp.M_ID
+                               select det_sol_comp;
+
+                return detalle_sol_comp.ToList<DETALLE_SOLICITUD_COMPRA>();
+            }
+        }
+
+        public static void Create(DETALLE_SOLICITUD_COMPRA det_sol_comp_new)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ctx.ListaSolicitudCompraDetalle.InsertOnSubmit(det_sol_comp_new);
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static DETALLE_SOLICITUD_COMPRA Read(int sc_mat_id, int m_mat_id)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                try
+                {
+                    var detalle_sol_comp = from det_sol_comp in ctx.ListaSolicitudCompraDetalle
+                                          where det_sol_comp.SC_ID.Equals(sc_mat_id) && det_sol_comp.M_ID.Equals(m_mat_id)
+                                          select det_sol_comp;
+                    return detalle_sol_comp.First<DETALLE_SOLICITUD_COMPRA>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void Update(DETALLE_SOLICITUD_COMPRA det_sol_comp_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                DETALLE_SOLICITUD_COMPRA detalle_sol_comp = (from det_sol_comp in ctx.ListaSolicitudCompraDetalle
+                                     where det_sol_comp.M_ID.Equals(det_sol_comp_upd.SC_ID) 
+                                     && det_sol_comp.SC_ID.Equals(det_sol_comp_upd.SC_ID)
+                                                     select det_sol_comp).First<DETALLE_SOLICITUD_COMPRA>();
+
+                detalle_sol_comp.DSC_CANTIDAD = det_sol_comp_upd.DSC_CANTIDAD;
+                
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static void Delete(DETALLE_SOLICITUD_COMPRA det_sol_comp_del)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                DETALLE_SOLICITUD_COMPRA borrar = (from det_sol_comp in ctx.ListaSolicitudCompraDetalle
+                                                   where det_sol_comp.M_ID.Equals(det_sol_comp_del.M_ID) 
+                                                   && det_sol_comp.SC_ID.Equals(det_sol_comp_del.SC_ID)
+                                                   select det_sol_comp).First<DETALLE_SOLICITUD_COMPRA>();
+
+                ctx.ListaSolicitudCompraDetalle.DeleteOnSubmit(borrar);
+                ctx.SubmitChanges();
+            }
+        }
     }
 }
