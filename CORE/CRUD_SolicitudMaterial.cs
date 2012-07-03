ab92e2518991c02d3084d4deb6d6d3f329428f61
kote <maria.osorios@usach.cl>
2012-07-02 20:50:04 -0400

    Solicitud Material y Solicitud Material Detalle

--- CORE/CRUD_SolicitudMaterial.cs
+++ CORE/CRUD_SolicitudMaterial.cs
@@ -7,5 +7,77 @@ namespace CORE
 {
     public class CRUD_SolicitudMaterial
     {
+
+        public static List<SOLICITUD_MATERIAL> getAll()
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                var solicitud_material = from sol_mat in ctx.ListaSolicitudMaterial
+                               
+                               orderby sol_mat.SM_ID
+                               select sol_mat;
+
+                return solicitud_material.ToList<SOLICITUD_MATERIAL>();
+            }
+        }
+
+        public static void Create(SOLICITUD_MATERIAL sol_mat_new)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ctx.ListaSolicitudMaterial.InsertOnSubmit(sol_mat_new);
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static SOLICITUD_MATERIAL Read(int sol_mat_id)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                try
+                {
+                    var solicitud_material = from sol_mat in ctx.ListaSolicitudMaterial
+                                   where sol_mat.SM_ID.Equals(sol_mat_id)
+                                   select sol_mat;
+                    return solicitud_material.First<SOLICITUD_MATERIAL>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void Update(SOLICITUD_MATERIAL sol_mat_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                SOLICITUD_MATERIAL solicitud_material = (from sol_mat in ctx.ListaSolicitudMaterial
+                                                         where sol_mat.SM_ID.Equals(sol_mat_upd.SM_ID)
+                                                         select sol_mat).First<SOLICITUD_MATERIAL>();
+
+                solicitud_material.E_ID = sol_mat_upd.E_ID;
+                solicitud_material.UNI_UC_ID = sol_mat_upd.UNI_UC_ID;
+                solicitud_material.UC_ID = sol_mat_upd.UC_ID;
+                solicitud_material.SM_TIPO = sol_mat_upd.SM_TIPO;
+                solicitud_material.SM_FECHA =sol_mat_upd.SM_FECHA;
+                solicitud_material.SM_ID_RECTIFICADA = sol_mat_upd.SM_ID_RECTIFICADA;
+
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static void Delete(SOLICITUD_MATERIAL sol_mat_del)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                SOLICITUD_MATERIAL borrar = (from sol_mat in ctx.ListaSolicitudMaterial
+                                             where sol_mat.SM_ID.Equals(sol_mat_del.SM_ID)
+                                             select sol_mat).First<SOLICITUD_MATERIAL>();
+
+                ctx.ListaSolicitudMaterial.DeleteOnSubmit(borrar);
+                ctx.SubmitChanges();
+            }
+        }
     }
 }
--- CORE/CRUD_SolicitudMaterialDetalle.cs
+++ CORE/CRUD_SolicitudMaterialDetalle.cs
@@ -7,5 +7,75 @@ namespace CORE
 {
     public class CRUD_SolicitudMaterialDetalle
     {
+
+        public static List<DETALLE_SOLICITUD_MATERIAL> getAll()
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                var solicitud_mat_det = from sol_mat_det in ctx.ListaSolicitudMaterialDetalle
+                               
+                               orderby sol_mat_det.SM_ID,sol_mat_det.M_ID
+                               select sol_mat_det;
+
+                return solicitud_mat_det.ToList<DETALLE_SOLICITUD_MATERIAL>();
+            }
+        }
+
+        public static void Create(DETALLE_SOLICITUD_MATERIAL sol_mat_det_new)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                ctx.ListaSolicitudMaterialDetalle.InsertOnSubmit(sol_mat_det_new);
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static DETALLE_SOLICITUD_MATERIAL Read(int sm_mat_det_id, int m_mat_det_id )
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                try
+                {
+                    var solicitud_mat_det = from sol_mat_det in ctx.ListaSolicitudMaterialDetalle
+                                   where sol_mat_det.SM_ID.Equals(sm_mat_det_id) 
+                                   && sol_mat_det.M_ID.Equals(m_mat_det_id)
+                                   select sol_mat_det;
+                    return solicitud_mat_det.First<DETALLE_SOLICITUD_MATERIAL>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void Update(DETALLE_SOLICITUD_MATERIAL sol_mat_det_upd)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                DETALLE_SOLICITUD_MATERIAL solicitud_mat_det = (from sol_mat_det in ctx.ListaSolicitudMaterialDetalle
+                                                                where sol_mat_det.M_ID.Equals(sol_mat_det_upd.M_ID) 
+                                                                && sol_mat_det.SM_ID.Equals(sol_mat_det_upd.SM_ID)
+                                                                select sol_mat_det).First<DETALLE_SOLICITUD_MATERIAL>();
+
+                solicitud_mat_det.DSM_CANTIDAD = sol_mat_det_upd.DSM_CANTIDAD;
+                
+                ctx.SubmitChanges();
+            }
+        }
+
+        public static void Delete(DETALLE_SOLICITUD_MATERIAL sol_mat_det_del)
+        {
+            using (BODEXDataContext ctx = new BODEXDataContext())
+            {
+                DETALLE_SOLICITUD_MATERIAL borrar = (from sol_mat_det in ctx.ListaSolicitudMaterialDetalle
+                                                    where sol_mat_det.M_ID.Equals(sol_mat_det_del.M_ID) 
+                                                      && sol_mat_det.SM_ID.Equals(sol_mat_det_del.SM_ID)
+                                                     select sol_mat_det).First<DETALLE_SOLICITUD_MATERIAL>();
+
+                ctx.ListaSolicitudMaterialDetalle.DeleteOnSubmit(borrar);
+                ctx.SubmitChanges();
+            }
+        }
     }
 }
